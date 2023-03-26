fb057a4fe61330428d99458a94cc2affa411d30d
marovichn <nikolamarovic.11@gmail.com>
2023-03-26 17:16:43 +0200

    using-UseEffect-hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,6 +7,10 @@ function App() {
   const [fetchedMovies, setFetchedMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
 
   //fetching data, not dummy
   async function fetchMoviesHandler() {
